2d435933a82e085eb79147ee14439fcd9d3d59ad
jmorgandev <22511129+jmorgandev@users.noreply.github.com>
2024-04-13 23:10:35 +0100

    move player.gd script, pan camera with mouse drag

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,11 @@
 extends Camera3D
 
 var is_dragging := false
-var last_drag_position := Vector2.ZERO
-var drag_delta := Vector2.ZERO
-
 const DRAG_SENSITVITY := 0.02
 
 var view_forward: Vector3
 var view_right: Vector3
+var view_forward_compensation: float = 90.0 / abs(rotation_degrees.x) * 0.8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,27 +15,16 @@ func _ready():
 
 func _unhandled_input(event: InputEvent):
     if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_RIGHT:
+        if event.button_index == MOUSE_BUTTON_LEFT:
             is_dragging = event.pressed
-            last_drag_position = event.position
-            drag_delta = Vector2.ZERO
     elif event is InputEventMouseMotion and is_dragging:
-        drag_delta = (last_drag_position - event.position)
-        last_drag_position = event.position
+        position += ((view_forward * -event.relative.y * view_forward_compensation) + (view_right * -event.relative.x)) * DRAG_SENSITVITY
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     var view_movement := Vector3.ZERO
-    var view_forward := transform.basis.z.normalized()
-    
-    var view_right := transform.basis.x.normalized()
-    var view_forward_compensation: float = 90.0 / abs(rotation_degrees.x) * 0.8
-       
-    if is_dragging:
-        print(drag_delta)
-        view_movement = ((view_forward * drag_delta.y) + (view_right * drag_delta.x))
-        position += view_movement * delta 
-    else:
+
+    if not is_dragging:
         var input_dir := Input.get_vector("left", "right", "up", "down").normalized()
         view_movement = (view_forward * input_dir.y * view_forward_compensation) + (view_right * input_dir.x) * 10.0
         position += view_movement * delta
