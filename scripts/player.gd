c8e631802edcb5691fb67f04a162162cdef0f92f
jmorgandev <22511129+jmorgandev@users.noreply.github.com>
2024-04-14 00:18:29 +0100

    mouse to world projection

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends Camera3D
 
+@onready var raycast: RayCast3D = $RayCast3D
+var is_selection_queued := false
+
 var is_dragging := false
 const DRAG_SENSITVITY := 0.02
 
@@ -13,9 +16,19 @@ func _ready():
     view_forward.y = 0.0 #ignore the Y component since the camera is looking down
     view_right = transform.basis.x.normalized()
 
+func _queue_selection_from_mouse_position(mouse_position: Vector2):
+    var from = project_ray_origin(mouse_position)
+    var to = from + (project_ray_normal(mouse_position) * 100.0)
+    raycast.global_position = from
+    raycast.target_position = raycast.to_local(to)
+    raycast.force_raycast_update()
+    is_selection_queued = true
+
 func _unhandled_input(event: InputEvent):
     if event is InputEventMouseButton:
-        if event.button_index == MOUSE_BUTTON_LEFT:
+        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+            _queue_selection_from_mouse_position(event.position)
+        elif event.button_index == MOUSE_BUTTON_RIGHT:
             is_dragging = event.pressed
     elif event is InputEventMouseMotion and is_dragging:
         position += ((view_forward * -event.relative.y * view_forward_compensation) + (view_right * -event.relative.x)) * DRAG_SENSITVITY
@@ -29,4 +42,13 @@ func _process(delta):
         view_movement = (view_forward * input_dir.y * view_forward_compensation) + (view_right * input_dir.x) * 10.0
         position += view_movement * delta
     
+func _process_selection():
+    pass
     
+func _physics_process(delta):
+    if raycast.enabled and is_selection_queued:
+        is_selection_queued = false
+        if raycast.is_colliding():
+            var collider := raycast.get_collider() as Node3D
+            print("found an object at ", collider.global_position)
+        
